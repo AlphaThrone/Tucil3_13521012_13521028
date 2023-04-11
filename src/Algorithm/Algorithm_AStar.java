626fc6f3dc14e97625e7169fd2f35c8eab378b34
AlphaThrone <haikalardzi@gmail.com>
2023-04-11 12:33:17 +0700

    Contributor: @zulfiansyah404 feat: base algo method

---
+++ src/Algorithm/Algorithm_AStar.java
@@ -0,0 +1,10 @@
+package Algorithm;
+
+
+
+public class Algorithm_AStar extends Algorithm {
+    
+    public void pathfind(){
+        /*TODO: AStar Algorithm */
+    }
+}
---
+++ src/Algorithm/Algorithm_UCS.java
@@ -0,0 +1,8 @@
+package Algorithm;
+
+public class Algorithm_UCS extends Algorithm {
+    
+    public void pathfind(){
+        /*TODO: UCS Algorithm */
+    }
+}
